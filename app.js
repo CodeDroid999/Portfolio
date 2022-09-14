4db011f00b343156b9b297d1cbf5a208df823d86
CodeDroid999 <johnmwega2002@gmail.com>
2022-09-14 22:45:40 +0300

    typewriter effect on send button

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-//form valiadtion
+//form validatiion
 function validateForm() {
     let x = document.forms["myForm"]["fname"].value;
     if (x == "") {
@@ -7,3 +7,16 @@ function validateForm() {
       return false;
     }
   }
+
+  //typewriter effect
+  var i = 0;
+var txt = 'Thank you for the message...talk to you soon!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("thanks").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
